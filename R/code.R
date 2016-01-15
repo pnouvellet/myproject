ee64b945f80877a15b0d1d5959463d2d5ab886aa
Christophe <c.fraser@imperial.ac.uk>
2016-01-15 16:19:02 +0000

    Oh no conflict, resolved

--- R/code.R
+++ R/code.R
@@ -1,4 +1,18 @@
 hawefk
 
+
+
 Tini's edits
+
+
+
+=======
+hawefk <- function(x) {
+  print(
+    paste("Hello Pierre, what do you think about", x, "?", sep=" ") 
+  )
+}
+
+hawefk("Donald Trump")
+
 
